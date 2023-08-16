97389940ffaad3240f9b475cb14c4d8c91c48381
JCuack <tovar.cuack@gmail.com>
2023-08-16 16:32:33 -0600

    5159515

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,8 @@
-from app import app 
+from flask import render_template
+from app import app
+
 @app.route('/') 
-@app.route('/index') 
-def index (): 
-    user = {'username' : 'Miguel'} 
-    return '''
-<html>
-<head>
-<title>HomePage - Microblog</title>
-</head>
-<body>
-<h1>Hello,''' + user['username'] + '''!</h1>
-</body>
-</html>'''
+@app.route('/index')
+def index():
+    user = {'username':'Miguel'}
+    return render_template('index.html',title='Home',user=user)
